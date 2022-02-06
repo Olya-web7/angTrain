00f720ba01047e5905389f3beab4b8a7629c3eb3
Olya <ola.shy@yahoo.com>
2022-02-06 14:45:28 +0200

    fix

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,12 +6,13 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {name: string, type: string, content: string};
+  @Input() element: { type: string, name: string, content: string };
+  // serverElements = [{ type: 'server', name: 'testserver', content: 'just some text' }];
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.element);
+    // console.log(this.element);
   }
 
 }
